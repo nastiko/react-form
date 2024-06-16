db44891f1c214e6000015562681e5240c342d403
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-06-16 12:18:45 +0100

    completed creating the "validation form"

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,22 +6,25 @@ import * as yup from "yup";
 export default function Form() {
     const schema = yup.object().shape({
         firstName: yup.string().required("First name is required"),
-        lastName: yup.string().required(),
-        email: yup.string().email().required(),
-        password: yup.string().required(),
-        phone: yup.string().required()
+        lastName: yup.string().required("Last name is required"),
+        email: yup.string().email().required("Email is required"),
+        phone: yup.string().required("Phone is required")
             .matches(/^((0|\+44)7\d{3}\s?\d{6})$/, 'Provide a valid UK phone number')
             .min(11, 'Phone number must be at least 11 digits')
             .max(13, 'Phone number must not exceed 13 digits'),
-        description: yup.string().required(),
+        description: yup.string().required("Message is required"),
         agreeTerms: yup.bool().oneOf([true], 'You must accept the terms and conditions').required()
     });
 
+    const [formSuccess, setFormSuccess] = React.useState(false);
     const { register, handleSubmit, formState: {errors} } = useForm({
         resolver: yupResolver(schema)
     });
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        console.log(data);
+        setFormSuccess(true);
+    };
 
     return (
         <>
@@ -30,8 +33,9 @@ export default function Form() {
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="mb-4">
                         <input
-                            className="w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent border-b-[#d9d9d9] rounded-none py-2.5
-                            focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:border-b-[#000000] focus:ring-0 focus:outline-none"
+                            className={`w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent rounded-none py-2.5
+                            focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:ring-0 focus:outline-none
+                            ${errors.firstName ? 'border-b-[#ff3860] focus:border-b-[#ff3860]' : 'border-b-[#d9d9d9] focus:border-b-[#000000]'}`}
                             type="text"
                             placeholder="First name"
                             {...register("firstName")}
@@ -40,36 +44,43 @@ export default function Form() {
                     </div>
                     <div className="mb-4">
                         <input
-                            className="w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent border-b-[#d9d9d9] rounded-none py-2.5
-                            focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:border-b-[#000000] focus:ring-0 focus:outline-none"
+                            className={`w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent rounded-none py-2.5
+                            focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:ring-0 focus:outline-none
+                            ${errors.lastName ? 'border-b-[#ff3860] focus:border-b-[#ff3860]' : 'border-b-[#d9d9d9] focus:border-b-[#000000]'}`}
                             type="text"
                             placeholder="Last name"
                             {...register("lastName")}
                         />
+                        <p className="text-[12px] text-[#ff3860] mt-2">{errors.lastName?.message}</p>
                     </div>
                 </div>
                 <div className="mb-4">
                     <input
-                        className="w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent border-b-[#d9d9d9] rounded-none py-2.5
-                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:border-b-[#000000] focus:ring-0 focus:outline-none"
+                        className={`w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent rounded-none py-2.5
+                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:ring-0 focus:outline-none
+                        ${errors.email ? 'border-b-[#ff3860] focus:border-b-[#ff3860]' : 'border-b-[#d9d9d9] focus:border-b-[#000000]'}`}
                         type="email"
                         placeholder="Email"
                         {...register("email")}
                     />
+                    <p className="text-[12px] text-[#ff3860] mt-2">{errors.email?.message}</p>
                 </div>
                 <div className="mb-4">
                     <input
-                        className="w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent border-b-[#d9d9d9] rounded-none py-2.5
-                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:border-b-[#000000] focus:ring-0 focus:outline-none"
+                        className={`w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent rounded-none py-2.5
+                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:ring-0 focus:outline-none
+                        ${errors.phone ? 'border-b-[#ff3860] focus:border-b-[#ff3860]' : 'border-b-[#d9d9d9] focus:border-b-[#000000]'}`}
                         type="phone"
                         placeholder="Phone"
                         {...register("phone")}
                     />
+                    <p className="text-[12px] text-[#ff3860] mt-2">{errors.phone?.message}</p>
                 </div>
                 <div className="mb-4">
                     <textarea
-                        className="w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent border-b-[#d9d9d9] rounded-none py-2.5
-                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:border-b-[#000000] focus:ring-0 focus:outline-none"
+                        className={`w-full text-[#495057] bg-[#ffffff] border-[1px] border-x-transparent border-t-transparent rounded-none py-2.5
+                        focus:border-[1px] focus:border-x-transparent focus:border-t-transparent focus:ring-0 focus:outline-none
+                        ${errors.description ? 'border-b-[#ff3860] focus:border-b-[#ff3860]' : 'border-b-[#d9d9d9] focus:border-b-[#000000]'}`}
                         placeholder="Write your message"
                         cols="30"
                         rows="7"
@@ -77,6 +88,7 @@ export default function Form() {
                         {...register("description")}
                     >
                     </textarea>
+                    <p className="text-[12px] text-[#ff3860]">{errors.description?.message}</p>
                 </div>
                 <div className="mb-4">
                     <input
@@ -84,9 +96,11 @@ export default function Form() {
                         type="checkbox"
                         {...register("agreeTerms")}
                     />
-                    <label>I understood the policy company</label>
+                    <label>I understood the <a className="underline" href="#">policy company</a></label>
+                    <p className="text-[12px] text-[#ff3860]">{errors.agreeTerms?.message}</p>
                 </div>
-                <button className="w-max h-[55px] bg-[#eb4d55] text-[16px] leading-[24px] text-[#ffffff] font-normal border-[1px] border-[#eb4d55] transition-all py-2 px-6 hover:bg-[#e72b34] hover:border-[#e61f29]">Send message</button>
+                <button type="submit" className="w-max h-[55px] bg-[#eb4d55] text-[16px] leading-[24px] text-[#ffffff] font-normal border-[1px] border-[#eb4d55] transition-all py-2 px-6 hover:bg-[#e72b34] hover:border-[#e61f29]">Send message</button>
+                {formSuccess && <p className="mt-[25px]">Thanks for your submission!</p>}
             </form>
         </>
     )
